71033c28712897f505ead768ad1f5c2c0e93e4f6
RiccardoTancredi <ric.tancredi@gmail.com>
2024-07-07 18:50:06 +0200

    Code update for Figure1 and Figure2
    Plot of Hamiltonian evolution
    Analysis code correction for the correct computation of ΔF(t) and PMF(z)

--- analysis.py
+++ analysis.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from scipy.optimize import newton
-plt.rcParams.update({'font.size': 12.5})
+from tqdm import tqdm
+plt.rcParams.update({'font.size': 12})
 
 folder = 'res/working/'
 ext = '.dat'
 show = False
-do_computation_PMF = False
-save_pics = False
+do_computation_PMF = True
+save_pics = True
 beta = 1.
 N = 500
 n_F = n_R = N
@@ -34,21 +35,21 @@ def Bidirectional_Method(ΔF, W_F_t, W_B_t, W_F, W_B):
     return -np.log(np.mean(np.exp(-W_F_t)/(1.+np.exp(-(W_F-ΔF)))) + np.mean(np.exp(-(W_B_t+ΔF))/(1.+np.exp(-(W_B+ΔF)))))
 
 W_f = np.loadtxt(folder + 'FORWARD_' + f'Work' + ext)
-W_b = np.loadtxt(folder + 'BACKWARD_' + f'Work' + ext)
+W_b = np.loadtxt(folder + 'BACKWARD_' + f'Work' + ext)[::-1]
 
-Work = (np.mean(W_f[-1, :]) + np.mean(W_b[-1, :]))/2
+Work = (np.mean(W_f[-1, :]) + np.mean(W_b[0, :]))/2
 print(f'Work = {Work}')
 
 print(f'Var W_f = {np.var(W_f[-1, :])}\nmean = {np.mean(W_f[-1, :])}\nmean(exp) = {np.mean(np.exp(-W_f[-1, :]))}')
-print(f'Var W_b = {np.var(W_b[-1, :])}\nmean = {np.mean(W_b[-1, :])},\nmean(exp) = {np.mean(np.exp(-W_b[-1, :]))}')
+print(f'Var W_b = {np.var(W_b[0, :])}\nmean = {np.mean(W_b[0, :])},\nmean(exp) = {np.mean(np.exp(-W_b[0, :]))}')
 
 tot_work_F = W_f[-1, :]
-tot_work_B = W_b[-1, :]
+tot_work_B = W_b[0, :]
 
 ΔF = newton(f, x0=5, args=(tot_work_F, tot_work_B), maxiter=1000)
 print(f'At time τ: BAR method outputs ΔF = {ΔF}')
 
-Delta_F_t = np.array([Bidirectional_Method(ΔF, W_f[t, :], W_b[t, :], tot_work_F.mean(), tot_work_B.mean()) for t in range(tot_records)])
+Delta_F_t = np.array([Bidirectional_Method(ΔF, W_f[t, :], W_b[t, :], tot_work_F, tot_work_B) for t in range(tot_records)])
 
 if show:
     nf, bins_f = np.histogram(tot_work_F, density=True, bins=30)
@@ -70,8 +71,11 @@ if show:
     plt.show()
 
 Jarz_eq_f = -np.log(np.mean(np.exp(-W_f), axis=1))
-Jarz_eq_b = -np.log(np.mean(np.exp(-W_b), axis=1)) + ΔF                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+Jarz_eq_b = -np.log(np.mean(np.exp(-W_b), axis=1)) + ΔF
 
+#####################
+### Plot FIGURE 1 ###
+#####################
 
 time_steps = 10
 plt.figure(figsize=(10, 4))
@@ -87,7 +91,8 @@ plt.ylabel(r'$\Delta F_t\: (k_B T)$')
 plt.tight_layout()
 if save_pics:
     plt.savefig('imgs/Figure1.png', dpi=500, transparent=True)
-plt.show()
+    print('Figure1.png saved!')
+plt.show(block=False)
 
 
 
@@ -104,6 +109,7 @@ def V_t(z, z_t):
 def Hamiltonian(z, z_t):
     return H0(z) + V_t(z, z_t)
 
+W_b = W_b[::-1] # reset sorting
 
 z_t_forw = np.linspace(-1.5, 1.5, tot_records)#.tolist() + [1.5]
 z_t_back = np.linspace(1.5, -1.5, tot_records)#.tolist() + [-1.5]
@@ -112,7 +118,7 @@ rho0_f = np.zeros(z_t_forw.shape)
 rho0_b = np.zeros(z_t_back.shape)
 
 data_f = np.loadtxt(folder + 'FORWARD_data.dat')[1:, :]
-data_b = np.loadtxt(folder + 'BACKWARD_data.dat')[1:, :]
+data_b = np.loadtxt(folder + 'BACKWARD_data.dat')[1:, :][::-1, :]
 
 average_W_f = np.mean(W_f, axis=1)
 average_W_b = np.mean(W_b, axis=1)
@@ -127,33 +133,38 @@ pmf_b = np.zeros(num_bins)
 pmf_good = np.zeros(num_bins)
 delta = (lambda_max - lambda_min) / num_bins
 
+
 if not os.path.exists(folder + 'PMF_F.dat') or do_computation_PMF:
 
-    for i in range(num_bins):
+    for i in tqdm(range(num_bins), colour='green'):
         lambda_i = lambda_bins[i]
         # lambda_i_b = lambda_bins[num_bins-1-i]
         num, num_b, num_good_F, num_good_B = 0, 0, 0, 0
         den, den_b, den_good = 0, 0, 0
         for tt in range(tot_records):
             nf, bins_f = np.histogram(data_f[tt, :], density=True)
+            # _, n_workf = np.histogram(W_f[tt, :], density=True)
             real_bins_f = (bins_f[:-1]+bins_f[1:])/2
             mask_f = np.logical_and(real_bins_f >= lambda_i - delta/2, real_bins_f < lambda_i + delta/2)
             distrib_f = nf[mask_f]
-            
+            # work_f = ((n_workf[:-1]+n_workf[1:])/2)[mask_f]
+
             if sum(mask_f) > 0:
-                num += distrib_f*np.exp(-average_W_f[tt]) * np.exp(Jarz_eq_f[tt])
-                num_good_F += (distrib_f*np.exp(-average_W_f[tt])/(1.+np.exp(ΔF-average_W_f[-1])))*np.exp(Delta_F_t[tt])
+                num += np.mean(distrib_f*np.exp(-W_f[tt])) * np.exp(Jarz_eq_f[tt])
+                num_good_F += np.mean(distrib_f*np.exp(-W_f[tt])/(1.+np.exp(ΔF-W_f[-1])))*np.exp(Delta_F_t[tt])
             den += np.exp(Jarz_eq_f[tt] - V_t(lambda_i, z_t_forw[tt]))
-                
+
             nb, bins_b = np.histogram(data_b[tt, :], density=True)
+            # _, n_workb = np.histogram(W_b[tt, :], density=True)
             real_bins_b = (bins_b[:-1]+bins_b[1:])/2
             mask_b = np.logical_and(real_bins_b >= lambda_i - delta/2, real_bins_b < lambda_i + delta/2)
             distrib_b = nb[mask_b]
+            # work_b = ((n_workb[:-1]+n_workb[1:])/2)[mask_b]
 
             if sum(mask_b) > 0:
-                num_b += distrib_b*np.exp(-average_W_b[tt]-average_W_b[-1]) * np.exp(Jarz_eq_b[tt])
-                num_good_B += (distrib_b*np.exp(-average_W_b[tt]+average_W_b[-1])/(1.+np.exp((ΔF+average_W_b[-1]))))*np.exp(Delta_F_t[tt])
-            den_b += np.exp(Jarz_eq_b[tt] - V_t(lambda_i, z_t_back[tt]))
+                num_b += np.mean(distrib_b*np.exp(-(W_b[tt]))) * np.exp(Jarz_eq_b[tt] - ΔF)
+                num_good_B += np.mean(distrib_b*np.exp(W_b[-1]-W_b[::-1, :][tt])/(1.+np.exp((ΔF+W_b[-1]))))*np.exp(Delta_F_t[tt])
+            den_b += np.exp(Jarz_eq_b[tt] - V_t(lambda_i, z_t_forw[tt]))
 
             den_good += np.exp(Delta_F_t[tt] - V_t(lambda_i, z_t_forw[tt]))
             
@@ -161,18 +172,31 @@ if not os.path.exists(folder + 'PMF_F.dat') or do_computation_PMF:
         pmf_b[i] = -np.log(num_b/den_b)
         pmf_good[i] = -np.log((num_good_F + num_good_B)/den_good)
 
-    PMF_minimum = min(pmf.min(), pmf_good.min())
-
-    np.savetxt(folder + 'PMF_F' + ext, pmf-PMF_minimum)
-    np.savetxt(folder + 'PMF_B' + ext, pmf_b-PMF_minimum)
-    np.savetxt(folder + 'PMF_MA' + ext, pmf_good-PMF_minimum)
+    PMF_minimum =  min(pmf.min(), pmf_b.min(), pmf_good.min())
+    print(f'Minimum = {PMF_minimum}')
+    print(f'Minimum PMF_F = {pmf.min()}')
+    print(f'Minimum PMF_B = {pmf_b.min()}')
+    print(f'Minimum PMF_Good= {pmf_good.min()}')
+    pmf = pmf - PMF_minimum # pmf.min()
+    pmf_b = pmf_b - PMF_minimum # pmf_b.min()
+    pmf_good = pmf_good - PMF_minimum # pmf_good.min()
+    
+    np.savetxt(folder + 'PMF_F' + ext, pmf)
+    np.savetxt(folder + 'PMF_B' + ext, pmf_b)
+    np.savetxt(folder + 'PMF_MA' + ext, pmf_good)
     np.savetxt(folder + 'lambda_bins' + ext, lambda_bins)
 
 else:
+
     pmf = np.loadtxt(folder + 'PMF_F' + ext)
     pmf_b = np.loadtxt(folder + 'PMF_B' + ext)
     pmf_good = np.loadtxt(folder + 'PMF_MA' + ext)
     lambda_bins = np.loadtxt(folder + 'lambda_bins' + ext)
+
+
+#####################
+### Plot FIGURE 2 ###
+#####################
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 4))
 ts = 2
@@ -180,13 +204,19 @@ ax[0].scatter(lambda_bins[::ts], (pmf)[::ts], color='tab:green',
             marker=9, label='Forward: Unidirectional')
 ax[0].scatter(lambda_bins[::-ts], (pmf_b)[::ts], color='tab:red', 
             marker=8, label='Backward: Unidirectional')
+# ax[0].scatter(lambda_bins[::ts], (pmf_good)[::ts], color='tab:blue', 
+#             marker=10, label='Optimized: Bidirectional')
+
 ax[1].scatter(lambda_bins[::ts], (pmf_good)[::ts], color='tab:blue', 
             marker=10, label='Optimized: Bidirectional')
-ax[1].plot(lambda_bins[np.logical_and(lambda_bins > -1, lambda_bins <= 1.01)][::ts], 
-           V_t(lambda_bins, 0)[np.logical_and(lambda_bins > -1, lambda_bins <= 1.01)][::ts]+pmf_good[np.where(lambda_bins>0)[0][0]], 
+xx = np.linspace(-1, 1, 101)
+ax[1].plot(xx[::ts], V_t(xx[::ts], 0)+pmf_good[np.where(lambda_bins>0)[0][0]], 
            ls='--', color='orange')
 ax[0].set_ylabel(r'$PMF\: (k_B T)$')
 y_lims = ax[0].get_ylim()
+# print(y_lims)
+y_lims = (y_lims[0], max(20, y_lims[1]))
+ax[0].set_ylim(y_lims)
 ax[1].set_yticks([])
 ax[1].set_ylim(*y_lims)
 ax[0].legend()
@@ -196,4 +226,5 @@ plt.subplots_adjust(wspace=0.02, bottom=0.15)
 fig.text(0.5+0.01, 0.04, 'Position', ha='center', va='center', transform=fig.transFigure)
 if save_pics:
     plt.savefig('imgs/Figure2.png', dpi=500, transparent=True)
+    print('Figure2.png saved!')
 plt.show()
---
+++ hamiltonians.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from tqdm import tqdm
+import os
+plt.rcParams.update({'font.size': 12.5})
+
+saved_imgs_folder = 'imgs/gif/'
+
+def H0(z):
+    return 5*z**4 - 10*z**2 +3*z
+
+def Vt(z, z_t):
+    return 15/2*(z-z_t)**2
+
+def H(z, z_t):
+    return H0(z) + Vt(z, z_t)
+
+z_t = np.array(np.linspace(-1.5, 1.5, 750).tolist() + [1.5])
+zz = np.linspace(-1.5, 1.5, 500)
+
+if len(os.listdir(saved_imgs_folder)) == 0:
+    for tt in tqdm(range(len(z_t)+1)):
+        plt.plot(zz, list(map(H0, zz)), label=r'$H_0$', 
+                color='royalblue', ls=':')
+        plt.plot(zz, list(map(Vt, zz, np.zeros_like(zz))), 
+                label=r'$V_t$', color='darkorange', ls='--')
+        
+        if tt != 0:
+            plt.plot(zz, H(zz, z_t[tt-1]), label=r'$H_t$', color='limegreen')
+        
+        plt.ylim(-10, 15)
+        plt.grid(ls='--', alpha=0.5)
+        plt.legend()
+        plt.savefig(f'imgs/gif/Hamiltonian{tt}.png', dpi=150)
+        
+        plt.close()
+
+
+def make_gif(frame_folder, name='imgs/Hamiltonian.gif', jumps=8):
+    tot_range = len(os.listdir(frame_folder))
+    key_name, ext = os.listdir(frame_folder)[0].split('0')
+    images = [f'{frame_folder}{key_name}{ii}{ext}' for ii in range(tot_range)]
+    frames = [Image.open(image) for image in images][::jumps]
+    frames = frames + frames[::-1]
+    frame_one = frames[0]
+    frame_one.save(name, format="GIF", append_images=frames,
+                   save_all=True, duration=1e-3, loop=0)
+
+
+make_gif(saved_imgs_folder)
+for image in os.listdir(saved_imgs_folder):
+    os.remove(f'{saved_imgs_folder}{image}')
